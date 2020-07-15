a7f5768998a6c776a546ff4e7516ec0efb6b566f
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-15 18:24:27 +0800

    multiple players
    made some changes to enable multiple players to go against (for now) one enemy

--- main.py
+++ main.py
@@ -4,25 +4,42 @@ from gameclass.playerclass import player
 
 
 player1 = player("player1", 100, 50, 30, [])
+playerlist = [player1]                          # planning on including many players in the future
+
 enemy1 = player("enemy1", 100, 50, 30, [])
+
 running = 0
 while running == 0:
-    player1.statsbar()
+    zerohpplayers = 0          # counts how many of our players are dead
+    
+    for player in playerlist:
+        player.statsbar()
     enemy1.statsbar()
 
-    player1.actions()
-    actioninput = input("choose action\n")
-    if int(actioninput) == 1:
-        player1damage = random.randrange(0.9 * player1.dmg, 1.1 * player1.dmg)          # generate damage 0.9 - 1.1 of our player's 
-        enemy1.dmgreceive(player1damage)                                                # base damage
-        print("player1 dealt", player1damage, "damage")
-        if enemy1.hp == 0:
-            print("you won")
-            break                               # code stops when enemy1 reaches 0 hp first
-    player1.dmgreceive(enemy1.dmg)
-    if player1.hp == 0:
-        running = 1
-        print("you lost")                       # same as above, except while loop stops since running != 0
+    for player in playerlist:
+        player.actions()
+        actioninput = input("choose action\n")
+        if int(actioninput) == 1:
+            # basic attack
+            playerdamage = random.randrange(0.9 * player.dmg, 1.1 * player.dmg)          # generate damage 0.9 - 1.1 of our player's 
+            enemy1.dmgreceive(playerdamage)                                                # base damage
+            print("player1 dealt", playerdamage, "damage")
+            if enemy1.hp == 0:
+                print("you won")
+                running = 1                              # code stops when enemy1 reaches 0 hp first
+    if enemy1.hp > 0:
+         targetplayerindex = random.randrange(0, len(playerlist))       # targets random player in playerlist
+         playerlist[targetplayerindex].dmgreceive(enemy1.dmg)   
+     
+     for player in playerlist:
+        # checks how many of our party members are dead every loop
+        if player.hp == 0:
+            zerohpplayers += 1
+     
+     if zerohpplayers == len(playerlist):
+     # set running to 1 which breaks the loop, when everyone in our party is dead
+         running = 1
+         print("you lost")                       
 
 
 
