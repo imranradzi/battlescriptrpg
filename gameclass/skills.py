ed07dba434313cf3afbe49a9f370b8463593eef4
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-19 11:06:35 +0800

    added file containing skills

---
+++ gameclass/skills.py
@@ -0,0 +1,8 @@
+# variable of skills which are dictionaries
+
+
+skill1 = {'name': 'skill1', 'type': 'attack', 'dmg': 50, 'mpcost': 20, 'description': 'basic damage skill'}
+# basic attack type skill
+
+heal1 = {'name': 'heal1', 'type': 'heal', 'dmg': 50, 'mpcost': 70, 'description': 'basic healing skill'}
+# basic heal type skill
