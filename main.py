f1e41ee9e86e3746ac23a3fcb34be2eb1c698fe3
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-19 23:43:35 +0800

    bug fixing
    made it so only players with greater than 0 hp could do actions, and also created a list of players that are alive for the enemy to attack, since previously the enemy could constantly attack players with 0 hp

--- main.py
+++ main.py
@@ -22,44 +22,50 @@ while running == 0:
     playerrun = 0
     while playerrun == 0:
         for player in playerlist:
-            player.actions()
-            actioninput = input('choose action\n')
-            if int(actioninput) == 1:
-                # basic attack
-                playerdamage = random.randrange(math.floor(0.9 * player.dmg), math.ceil(1.1 * player.dmg))
-                # generate damage 0.9 - 1.1 of our player's 
-                enemy1.dmgreceive(playerdamage)                                                
-                print(player.name, 'dealt', playerdamage, 'damage')
-                playerrun = 1   # player's action stops after performing an attack
-            elif int(actioninput) == 2:
-                player.skillindex()
-                skillinput = int(input('choose skill\n')) - 1
-                if player.mp > player.skills[skillinput]['mpcost']:
-                    if player.skills[skillinput]['type'] == 'attack':
-                        # check to see if our skill is an attack type or heal type
-                        player.manalose(player.skills[skillinput]['mpcost'])
-                        enemy1.dmgreceive(player.skills[skillinput]['dmg'])
-                        print(player.name, 'dealt', player.skills[skillinput]['dmg'], 'damage')
-                    elif player.skills[skillinput]['type'] == 'heal':
-                        player.manalose(player.skills[skillinput]['mpcost'])
-                        player.dmgheal(player.skills[skillinput]['dmg'])
-                        print(player.name, 'healed for', player.skills[skillinput]['dmg'], 'hp')
-                    playerrun = 1
+            if player.hp > 0: # player can only choose actions if they're alive
+                player.actions()
+                actioninput = input('choose action\n')
+                if int(actioninput) == 1:
+                    # basic attack
+                    playerdamage = random.randrange(math.floor(0.9 * player.dmg), math.ceil(1.1 * player.dmg))
+                    # generate damage 0.9 - 1.1 of our player's 
+                    enemy1.dmgreceive(playerdamage)                                                
+                    print(player.name, 'dealt', playerdamage, 'damage')
+                    playerrun = 1   # player's action stops after performing an attack
+                elif int(actioninput) == 2:
+                    player.skillindex()
+                    skillinput = int(input('choose skill\n')) - 1
+                    if player.mp > player.skills[skillinput]['mpcost']:
+                        if player.skills[skillinput]['type'] == 'attack':
+                            # check to see if our skill is an attack type or heal type
+                            player.manalose(player.skills[skillinput]['mpcost'])
+                            enemy1.dmgreceive(player.skills[skillinput]['dmg'])
+                            print(player.name, 'dealt', player.skills[skillinput]['dmg'], 'damage')
+                        elif player.skills[skillinput]['type'] == 'heal':
+                            player.manalose(player.skills[skillinput]['mpcost'])
+                            player.dmgheal(player.skills[skillinput]['dmg'])
+                            print(player.name, 'healed for', player.skills[skillinput]['dmg'], 'hp')
+                        playerrun = 1
+                    else:
+                        print('not enough mp, back to action list')
+                        # playerrun isn't set to 1, so loop continues and player keeps selecting action
                 else:
-                    print('not enough mp, back to action list')
-                    # playerrun isn't set to 1, so loop continues and player keeps selecting action
-            else:
-                # when player doesn't want to do anything
-                playerrun = 1
-            if enemy1.hp == 0:
-                    print('you won')
-                    running = 1  # code stops when enemy1 reaches 0 hp first
-            
-                
+                    # when player doesn't want to do anything
+                    playerrun = 1
+                if enemy1.hp == 0:
+                        print('you won')
+                        running = 1  # code stops when enemy1 reaches 0 hp first
+                        
+    attacklist = []
+    for player in playerlist:
+        # some players might have 0 hp, so we create a list of attackable players for the enemy
+        # which consists of players with hp greater than 0
+        if player.hp > 0:
+            attacklist.append(player)
                 
     if enemy1.hp > 0:
-         targetplayerindex = random.randrange(0, len(playerlist))       # targets random player in playerlist
-         playerlist[targetplayerindex].dmgreceive(enemy1.dmg)   
+         targetplayerindex = random.randrange(0, len(attacklist))       # targets random player in playerlist
+         attacklist[targetplayerindex].dmgreceive(enemy1.dmg)   
      
     for player in playerlist:
         # checks how many of our party members are dead every loop
