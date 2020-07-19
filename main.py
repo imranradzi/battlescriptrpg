e537dc352020b5ab860fbca5abebf1290e38163c
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-19 23:15:43 +0800

    added loop for player actions
    player might not have mp to do certain skills, in which case loops should be used until player actually chooses to do something that's within mana range/basic attack (or do nothing at all)

--- main.py
+++ main.py
@@ -17,36 +17,41 @@ while running == 0:
         player.statsbar()
     enemy1.statsbar()
 
-    for player in playerlist:
-        player.actions()
-        actioninput = input('choose action\n')
-        if int(actioninput) == 1:
-            # basic attack
-            playerdamage = random.randrange(0.9 * player.dmg, 1.1 * player.dmg)          # generate damage 0.9 - 1.1 of our player's 
-            enemy1.dmgreceive(playerdamage)                                                # base damage
-            print('player1 dealt', playerdamage, 'damage')
-        elif int(actioninput) == 2:
-            player.skillindex()
-            skillinput = int(input('choose skill\n')) - 1
-            runnableskills = 0 # counter of how many skills we can do considering our mp
-            for i in player.skills:          
-                if player.mp >= i['mpcost']:
-                    runnableskills += 1
-            if runnableskills > 0:
-                if player.skills[skillinput]['type'] == 'attack':
-                    # check to see if our skill is an attack type or heal type
-                    player.manalose(player.skills[skillinput]['mpcost'])
-                    enemy1.dmgreceive(player.skills[skillinput]['dmg'])
-                    print(player.name, 'dealt', player.skills[skillinput]['dmg'], 'damage')
-                elif player.skills[skillinput]['type'] == 'heal':
-                    player.manalose(player.skills[skillinput]['mpcost'])
-                    player.dmgheal(player.skills[skillinput]['dmg'])
-                    print(player.name, 'healed for', player.skills[skillinput]['dmg'], 'hp')
+    playerrun = 0
+    while playerrun == 0:
+        for player in playerlist:
+            player.actions()
+            actioninput = input('choose action\n')
+            if int(actioninput) == 1:
+                # basic attack
+                playerdamage = random.randrange(0.9 * player.dmg, 1.1 * player.dmg)          # generate damage 0.9 - 1.1 of our player's 
+                enemy1.dmgreceive(playerdamage)                                                # base damage
+                print('player1 dealt', playerdamage, 'damage')
+                playerrun = 1   # player's action stops after performing an attack
+            elif int(actioninput) == 2:
+                player.skillindex()
+                skillinput = int(input('choose skill\n')) - 1
+                if player.mp > player.skills[skillinput]['mpcost']:
+                    if player.skills[skillinput]['type'] == 'attack':
+                        # check to see if our skill is an attack type or heal type
+                        player.manalose(player.skills[skillinput]['mpcost'])
+                        enemy1.dmgreceive(player.skills[skillinput]['dmg'])
+                        print(player.name, 'dealt', player.skills[skillinput]['dmg'], 'damage')
+                    elif player.skills[skillinput]['type'] == 'heal':
+                        player.manalose(player.skills[skillinput]['mpcost'])
+                        player.dmgheal(player.skills[skillinput]['dmg'])
+                        print(player.name, 'healed for', player.skills[skillinput]['dmg'], 'hp')
+                    playerrun = 1
+                else:
+                    print('not enough mp, back to action list')
+                    # playerrun isn't set to 1, so loop continues and player keeps selecting action
             else:
-                print('not enough mp, proceeding to do nothing')
-    if enemy1.hp == 0:
-                print('you won')
-                running = 1  # code stops when enemy1 reaches 0 hp first
+                # when player doesn't want to do anything
+                playerrun = 1
+            if enemy1.hp == 0:
+                    print('you won')
+                    running = 1  # code stops when enemy1 reaches 0 hp first
+            
                 
                 
     if enemy1.hp > 0:
