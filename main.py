1b3a2a257dfe70616fed816ec565002a08839510
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-23 07:38:42 +0800

    formatting
    made some variable names shorter to shorten code length

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import gameclass.skills as gs
 from gameclass.playerclass import player
 
 
-player1 = player('player1', 100, 50, 30, [gs.skill1])
-player2 = player('player2', 100, 80, 15, [gs.heal1])
+player1 = player('player1', 100, 50, 10, 30, [gs.skill1])
+player2 = player('player2', 100, 80, 10, 15, [gs.heal1])
 playerlist = [player1, player2]                    
 
-enemy1 = player('enemy1', 100, 50, 30, [])
-enemy2 = player('enemy2', 100, 50, 30, [])
+enemy1 = player('enemy1', 100, 50, 10, 30, [])
+enemy2 = player('enemy2', 100, 50, 10, 30, [])
 enemylist = [enemy1, enemy2]
 
 running = 0
@@ -28,37 +28,37 @@ while running == 0:
             if player.hp > 0: # player can only choose actions if they're alive
                 player.actions()
                 actioninput = input('choose action\n')
-                enemyattacklist = []
+                enmatkl = []
                 for enemy in enemylist:
                     # at the start of every player's attack, create a list of enemies still alive
                     if enemy.hp > 0:
-                        enemyattacklist.append(enemy)
+                        enmatkl.append(enemy)
                 if int(actioninput) == 1:
                     # basic attack
                     playerdamage = random.randrange(math.floor(0.9 * player.dmg), math.ceil(1.1 * player.dmg))
                     # generate damage 0.9 - 1.1 of our player's
-                    for enemy in enemyattacklist:
-                        print(str(enemyattacklist.index(enemy) + 1) + ':' + enemy.name) # print list of enemies player can attack
-                    enemyattackindex = int(input('choose an enemy to attack')) - 1
-                    enemyattacklist[enemyattackindex].dmgreceive(playerdamage)                                              
+                    for enemy in enmatkl:
+                        print(str(enmatkl.index(enemy) + 1) + ':' + enemy.name) # print list of enemies player can attack
+                    enmatkind = int(input('choose an enemy to attack')) - 1
+                    enmatkl[enmatkind].dmgreceive(playerdamage)                                              
                     print(player.name, 'dealt', playerdamage, 'damage')
                     playerrun = 1   # player's action stops after performing an attack
                 elif int(actioninput) == 2:
                     player.skillindex()
-                    skillinput = int(input('choose skill\n')) - 1
-                    if player.mp > player.skills[skillinput]['mpcost']:
-                        if player.skills[skillinput]['type'] == 'attack':
+                    sklinput = int(input('choose skill\n')) - 1
+                    if player.mp > player.skills[sklinput]['mpcost']:
+                        if player.skills[sklinput]['type'] == 'attack':
                             # check to see if our skill is an attack type or heal type
-                            player.manalose(player.skills[skillinput]['mpcost'])
-                            for enemy in enemyattacklist:
-                                print(str(enemyattacklist.index(enemy) + 1) + ':' + enemy.name)
-                            enemyattackindex = int(input('choose an enemy to attack')) - 1
-                            enemyattacklist[enemyattackindex].dmgreceive(player.skills[skillinput]['dmg'])
-                            print(player.name, 'dealt', player.skills[skillinput]['dmg'], 'damage')
-                        elif player.skills[skillinput]['type'] == 'heal':
-                            player.manalose(player.skills[skillinput]['mpcost'])
-                            player.dmgheal(player.skills[skillinput]['dmg'])
-                            print(player.name, 'healed for', player.skills[skillinput]['dmg'], 'hp')
+                            player.manalose(player.skills[sklinput]['mpcost'])
+                            for enemy in enmatkl:
+                                print(str(enmatkl.index(enemy) + 1) + ':' + enemy.name)
+                            enmatkind = int(input('choose an enemy to attack')) - 1
+                            enmatkl[enmatkind].dmgreceive(player.skills[sklinput]['dmg'])
+                            print(player.name, 'dealt', player.skills[sklinput]['dmg'], 'damage')
+                        elif player.skills[sklinput]['type'] == 'heal':
+                            player.manalose(player.skills[sklinput]['mpcost'])
+                            player.dmgheal(player.skills[sklinput]['dmg'])
+                            print(player.name, 'healed for', player.skills[sklinput]['dmg'], 'hp')
                         playerrun = 1
                     else:
                         print('not enough mp, back to action list')
@@ -69,8 +69,8 @@ while running == 0:
                 for enemy in enemylist:
                     # we check if there is any enemy with positive hp, if not, then players win
                     if enemy.hp > 0:
-                        enemyattacklist.append(enemy)
-                if enemyattacklist == []:           # if there are no enemies to attack, players have won
+                        enmatkl.append(enemy)
+                if enmatkl == []:           # if there are no enemies to attack, players have won
                         print('you won')
                         running = 1  # code stops when enemy1 reaches 0 hp first
                         break             
