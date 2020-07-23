2ef72e15fef81bf44a4e08ac51df5780f84b090b
imranradzi <67956145+imranradzi@users.noreply.github.com>
2020-07-23 22:05:30 +0800

    added dotdmg and dotheal

--- gameclass/skills.py
+++ gameclass/skills.py
@@ -6,3 +6,9 @@ skill1 = {'name': 'skill1', 'type': 'attack', 'dmg': 50, 'mpcost': 20, 'descript
 
 heal1 = {'name': 'heal1', 'type': 'heal', 'dmg': 50, 'mpcost': 70, 'description': 'basic healing skill'}
 # basic heal type skill
+
+dotdmg1 = {'name': 'dotdmg1', 'type': 'dotdmg', 'dmg': 30, 'mpcost': 35, 'description': 'basic dot dmg skill',
+           'turns': 2}
+
+dotheal1 = {'name': 'dotheal1', 'type': 'dotheal', 'dmg': 40, 'mpcost': 60, 'description': 'basic dot heal skill',
+            'turns': 2}
